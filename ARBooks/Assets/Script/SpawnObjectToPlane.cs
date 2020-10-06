ddc06b14486181b1bc02db996f5cfdeccc652f17
島袋太智 <e185712@e185712.local>
2020-10-06 15:15:51 +0900

    book touch

---
+++ ARBooks/Assets/Script/SpawnObjectToPlane.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class SpawnObjectToPlane : MonoBehaviour {
+    public GameObject cubePrefab;
+    GameObject spawnObject;
+    ARRaycastManager arRaycastManager;
+
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    // Start is called before the first frame update
+    void Start() {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.touchCount > 0) {
+            if (arRaycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon)) {
+                Pose hitPose = hits[0].pose;
+                if (spawnObject == null) {
+                    spawnObject = Instantiate(cubePrefab, hitPose.position, hitPose.rotation);
+                } else {
+                    spawnObject.transform.position = hitPose.position;
+                }
+            
+            }
+            
+        }
+        
+    }
+}
