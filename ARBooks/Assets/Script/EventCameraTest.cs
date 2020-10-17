d1b15bae018d0f54d9dcc7a20ac39c9844cd9367
島袋太智 <e185712@e185712.local>
2020-10-17 13:52:55 +0900

    ページがめくれる

---
+++ ARBooks/Assets/Script/EventCameraTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventCameraTest : MonoBehaviour {
+    Camera myCamera;
+    // Start is called before the first frame update
+    void Start() {
+        //canvas.GetComponent<Canvas> (). = Camera.main;
+        myCamera = GameObject.Find("AR Camera").GetComponent<Camera>();
+        Canvas theCanvas = GetComponent<Canvas>();
+        theCanvas.worldCamera = myCamera;
+        Debug.Log(theCanvas.worldCamera);
+    }
+
+    // Update is called once per frame
+}
